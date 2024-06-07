111cfbfd02bbeb5057e229c8d4d74e5c08bb98f6
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-07 13:13:57 -0400

    feat: :sparkles: add get entity middleware

---
+++ src/crud/middlewares/get-entity.middleware.ts
@@ -0,0 +1,23 @@
+import { Injectable, NestMiddleware, NotFoundException } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { DefaultDto } from '../dto';
+import { GenericPersistentEntity } from '../entity';
+import { GenericService } from '../services';
+
+@Injectable()
+export class LoadEntityMiddleware<
+  T extends GenericPersistentEntity,
+  D extends DefaultDto,
+> implements NestMiddleware
+{
+  constructor(private readonly service: GenericService<T, D>) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    const { id } = req.params;
+    const entity = await this.service.findOne(id);
+    if (!entity) {
+      throw new NotFoundException(`${entity.constructor.name} not found`);
+    }
+    Object.assign(req, { entity });
+    next();
+  }
+}
