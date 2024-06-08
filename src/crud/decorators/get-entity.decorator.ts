69b1cb596579b7d8fc09da9282fd46426be36196
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-08 17:53:05 -0400

    refactor: :recycle: rename decorator

--- src/crud/decorators/get-entity.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const GetEntity = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const { entity } = ctx.switchToHttp().getRequest();
-    console.log({ entity });
-    return entity;
-  },
-);
