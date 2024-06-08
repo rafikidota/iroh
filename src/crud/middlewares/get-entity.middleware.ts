4452d4051fa78e7a94c39b58b28e784946f09fb1
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-08 17:51:26 -0400

    fix: :bug: inject generic service

--- src/crud/middlewares/get-entity.middleware.ts
+++ src/crud/middlewares/get-entity.middleware.ts
@@ -1,4 +1,10 @@
-import { Injectable, NestMiddleware, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NestMiddleware,
+  NotFoundException,
+  Inject,
+  BadRequestException,
+} from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { DefaultDto } from '../dto/default.dto';
 import { GenericPersistentEntity } from '../entity/generic.persistent.entity';
@@ -10,10 +16,16 @@ export class GetEntityMiddleware<
   GenericDto extends DefaultDto,
 > implements NestMiddleware
 {
-  constructor(private readonly service: GenericService<Entity, GenericDto>) {}
+  constructor(
+    @Inject('ENTITY_SERVICE')
+    private readonly service: GenericService<Entity, GenericDto>,
+  ) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
     const { id } = req.params;
+    if (!id) {
+      throw new BadRequestException('id is required');
+    }
     const entity = await this.service.findOne(id);
     if (!entity) {
       throw new NotFoundException(`${entity.constructor.name} not found`);
