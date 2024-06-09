d513cfc94425b4334acbd6b987fd5853d1a2c787
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-09 01:25:41 -0400

    feat: :sparkles: add generic logger

---
+++ src/crud/logger/generic.logger.ts
@@ -0,0 +1,44 @@
+import { Injectable, Logger } from '@nestjs/common';
+import * as chalk from 'chalk';
+@Injectable()
+export class GenericLogger extends Logger {
+  startTime: number = Date.now();
+  constructor(context: string) {
+    super(context);
+  }
+
+  restart() {
+    this.startTime = Date.now();
+  }
+
+  getTimestamp() {
+    const now = Date.now() - this.startTime;
+    const timestamp = chalk.yellow(`+${now}ms`);
+    return timestamp;
+  }
+
+  post(message: string) {
+    const log = chalk.green(`[POST] ${message}`);
+    const timestamp = this.getTimestamp();
+    super.log(`${log} ${timestamp}`);
+  }
+
+  get(message: string) {
+    const log = chalk.magenta(`[GET] ${message}`);
+    const timestamp = this.getTimestamp();
+    super.log(`${log} ${timestamp}`);
+  }
+
+  patch(message: string) {
+    const log = chalk.cyan(`[PATCH] ${message}`);
+    const timestamp = this.getTimestamp();
+    super.log(`${log} ${timestamp}`);
+  }
+
+  delete(message: string) {
+    const log = chalk.red(`[DELETE] ${message}`);
+    const timestamp = this.getTimestamp();
+    super.log(`${log} ${timestamp}`);
+  }
+
+}
