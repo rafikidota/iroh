d160fadb814e50f568f8f287ec15ea5b9aa972ae
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-09 01:34:39 -0400

    feat: :sparkles: add logger exports

--- src/crud/index.ts
+++ src/crud/index.ts
@@ -2,5 +2,6 @@ export * from './controllers';
 export * from './decorators';
 export * from './dto';
 export * from './entity';
+export * from './logger';
 export * from './middlewares';
 export * from './services';
---
+++ src/crud/logger/index.ts
@@ -0,0 +1,1 @@
+export { GenericLogger } from './generic.logger';
