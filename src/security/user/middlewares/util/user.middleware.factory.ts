b60d87bdde71cf6db08fe0b64b5683cf4e0608eb
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-15 16:37:10 -0400

    fix: :bug: user middleware factory service type

--- src/security/user/middlewares/util/user.middleware.factory.ts
+++ src/security/user/middlewares/util/user.middleware.factory.ts
@@ -1,12 +1,12 @@
 import { Type, Provider } from '@nestjs/common';
-import { GenericService } from '../../../../crud';
 import { CreateGenericUserDto } from '../../dto';
 import { GenericUser } from '../../user.generic';
+import { GenericUserService } from '../../user.service';
 
-export function EntityMiddlewareFactory<
+export function UserMiddlewareFactory<
   T extends GenericUser,
   D extends CreateGenericUserDto,
->(service: Type<GenericService<T, D>>): Provider {
+>(service: Type<GenericUserService<T, D>>): Provider {
   return {
     provide: 'USER_MIDDLEWARE',
     useClass: service,
