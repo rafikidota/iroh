4eb8742d77b7cbc3cdeeeccd5832bc76bf1dc16d
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-15 21:14:55 -0400

    refactor: :recycle: minor changes

--- src/crud/guards/entity.guard.ts
+++ src/crud/guards/entity.guard.ts
@@ -1,20 +1,27 @@
-import { Injectable, CanActivate, ExecutionContext, BadRequestException, Inject } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  BadRequestException,
+  Inject,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GenericService } from '../services/generic.service';
 import { GenericPersistentEntity } from '../entity/generic.persistent.entity';
 import { DefaultDto } from '../dto/default.dto';
+import { FindOneOptions } from '../services';
 
 @Injectable()
 export class EntityGuard<
   Entity extends GenericPersistentEntity,
   GenericDto extends DefaultDto,
 > implements CanActivate
- {
+{
   constructor(
     private readonly reflector: Reflector,
     @Inject('GENERIC_SERVICE')
     private readonly service: GenericService<Entity, GenericDto>,
-  ) { }
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
@@ -22,7 +29,8 @@ export class EntityGuard<
     if (!id) {
       throw new BadRequestException('id is required');
     }
-    const entity = await this.service.findOne(id, { logging: false });
+    const options: FindOneOptions = { logging: false };
+    const entity = await this.service.findOne(id, options);
     Object.assign(req, { entity });
     return true;
   }
