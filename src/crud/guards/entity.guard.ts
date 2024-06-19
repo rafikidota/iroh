e3a7120379800c395854b8b412ff71461afee522
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-19 00:52:21 -0400

    fix: :bug: repository injection add Type <T>

--- src/crud/guards/entity.guard.ts
+++ src/crud/guards/entity.guard.ts
@@ -4,12 +4,19 @@ import {
   ExecutionContext,
   NotFoundException,
   InternalServerErrorException,
+  Type,
 } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class BuildEntityGuard<T> implements CanActivate {
-  constructor(private readonly repository: Repository<T>) {}
+  constructor(
+    @InjectRepository((): Type<T> => {
+      return {} as Type<T>;
+    })
+    protected repository: Repository<T>,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
