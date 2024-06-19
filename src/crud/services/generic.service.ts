a5befff1bbdf3cd45762e30f4212fb6b38a22525
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-18 22:40:57 -0400

    feat: :sparkles: apply interfaces to controller and service

--- src/crud/services/generic.service.ts
+++ src/crud/services/generic.service.ts
@@ -1,38 +1,36 @@
-import { DeepPartial, Repository } from 'typeorm';
+import { DeepPartial, FindOneOptions, Repository } from 'typeorm';
 import { SearchPaginateDto } from '../dto/search.paginate.dto';
 import { GenericPersistentEntity } from '../entity/generic.persistent.entity';
 import { NotFoundException } from '@nestjs/common';
-import { DefaultDto } from '../dto/default.dto';
 import { GenericLogger } from '../logger/generic.logger';
-import { FindOneOptions } from './util/find-one.options';
+import { LoggerOptions } from './util/logger.options';
 import { handleDatabaseError } from '../../common/util/error-handler';
 import { AppError } from '../../common/error/app.error';
+import { IGenericService } from 'src/intefaces/ICRUD.service';
 
 export class GenericService<
   Entity extends GenericPersistentEntity,
-  GenericDto extends DefaultDto = DefaultDto,
-> {
+  GenericDto extends DeepPartial<Entity>,
+> implements IGenericService<Entity, GenericDto>
+{
   protected logger: GenericLogger;
   private context: string;
-  constructor(
-    protected readonly repository: Repository<Entity> &
-      Repository<GenericPersistentEntity>,
-  ) {
+  constructor(protected readonly repository: Repository<Entity>) {
     this.context = `${repository.metadata.name}Logger`;
     this.logger = new GenericLogger(this.context);
   }
-  async create(createDto: DeepPartial<GenericDto>) {
+  async create(createDto: GenericDto): Promise<Entity> {
     try {
       this.logger.restart();
       const entity = await this.repository.save(createDto);
       this.logger.post(`[${entity.id}]`);
-      return entity;
+      return entity as unknown as Entity;
     } catch (error) {
       handleDatabaseError(error as AppError);
     }
   }
 
-  async paginate(query: SearchPaginateDto) {
+  async paginate(query: SearchPaginateDto): Promise<Entity[]> {
     try {
       this.logger.restart();
       const { limit, page } = query;
@@ -55,11 +53,12 @@ export class GenericService<
     }
   }
 
-  async findOne(id: string, options: FindOneOptions) {
+  async findOne(id: string, options: LoggerOptions): Promise<Entity> {
     try {
       this.logger.restart();
       const { name } = this.repository.metadata;
-      const entity = await this.repository.findOne({ where: { id } });
+      const where = { where: { id } } as unknown as FindOneOptions<Entity>;
+      const entity = await this.repository.findOne(where);
       if (!entity) {
         this.logger.warn(`[${id}] NOT FOUND`);
         throw new NotFoundException(`${name} with id ${id} not found`);
@@ -67,25 +66,28 @@ export class GenericService<
       if (options.logging) {
         this.logger.get(`[${entity.id}]`);
       }
-      return entity;
+      return entity as unknown as Entity;
     } catch (error) {
       handleDatabaseError(error as AppError);
     }
   }
 
-  async update(entity: Entity, updateDto: Partial<GenericDto>) {
+  async update(
+    entity: Entity,
+    updateDto: Partial<GenericDto>,
+  ): Promise<Entity> {
     try {
       this.logger.restart();
       Object.assign(entity, updateDto);
       const updatedEntity = await this.repository.save(entity);
       this.logger.patch(`[${updatedEntity.id}]`);
-      return updatedEntity;
+      return updatedEntity as unknown as Entity;
     } catch (error) {
       handleDatabaseError(error as AppError);
     }
   }
 
-  async remove(id: string) {
+  async remove(id: string): Promise<void> {
     try {
       this.logger.restart();
       await this.repository.softDelete(id);
