85015c5e3469bb7f6a683d715669dee73d7da026
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-19 01:40:35 -0400

    fix: :bug: entity guard return function

--- src/crud/guards/entity.guard.ts
+++ src/crud/guards/entity.guard.ts
@@ -1,40 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   Injectable,
   CanActivate,
   ExecutionContext,
   NotFoundException,
   InternalServerErrorException,
-  Type,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-@Injectable()
-export class BuildEntityGuard<T> implements CanActivate {
-  constructor(
-    @InjectRepository((): Type<T> => {
-      return {} as Type<T>;
-    })
-    protected repository: Repository<T>,
-  ) {}
+export function BuildEntityGuard<T>(E: new (...arg: any) => T) {
+  @Injectable()
+  class DynamicEntityGuard implements CanActivate {
+    repository: Repository<T>;
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const req = context.switchToHttp().getRequest();
-    const { id } = req.params;
+    constructor(@InjectRepository(E) repository: Repository<T>) {
+      this.repository = repository;
+    }
 
-    try {
-      const entity = await this.repository.findOne(id);
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+      const req = context.switchToHttp().getRequest();
+      const { id } = req.params;
 
-      if (!entity) {
-        throw new NotFoundException(
-          `${this.repository.metadata.name} with id ${id} not found`,
-        );
+      try {
+        const entity = await this.repository.findOne(id);
+
+        if (!entity) {
+          throw new NotFoundException(
+            `${this.repository.metadata.name} with id ${id} not found`,
+          );
+        }
+
+        req.entity = entity;
+        return true;
+      } catch (error) {
+        throw new InternalServerErrorException('Error while loading entity');
       }
-
-      req.entity = entity;
-      return true;
-    } catch (error) {
-      throw new InternalServerErrorException('Error while loading entity');
     }
   }
+
+  return DynamicEntityGuard;
 }
