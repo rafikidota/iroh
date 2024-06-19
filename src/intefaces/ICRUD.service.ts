abbc795a275d74cd0bd7c188ee567f9583564d03
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-18 22:38:23 -0400

    feat: :sparkles: add controller and service interface

---
+++ src/intefaces/ICRUD.service.ts
@@ -0,0 +1,11 @@
+import { DeepPartial } from 'typeorm';
+import { LoggerOptions, SearchPaginateDto } from '../crud';
+
+export interface IGenericService<T, D> {
+  create(createDto: DeepPartial<D>): Promise<T>;
+  paginate(query: SearchPaginateDto): Promise<T[]>;
+  findAll(): Promise<T[]>;
+  findOne(id: string, options: LoggerOptions): Promise<T>;
+  update(entity: T, updateDto: Partial<D>): Promise<T>;
+  remove(id: string): Promise<void>;
+}
