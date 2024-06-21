38e98407b128fe75af9bf07204911b518672a522
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-21 01:31:24 -0400

    fix: :bug: unresolved dependency

--- src/crud/controllers/generic.controller.ts
+++ src/crud/controllers/generic.controller.ts
@@ -11,7 +11,7 @@ import {
 } from '@nestjs/common';
 import { Entity, UseEntityGuard } from '../decorators';
 import { GenericPersistentEntity } from '../entity';
-import { BuildCRUDService, LoggerOptions } from '../services';
+import { LoggerOptions } from '../services';
 import { DeepPartial } from 'typeorm';
 import { SearchPaginateDto } from '../dto';
 import type { IGenericController, IGenericService } from '../interfaces';
@@ -20,11 +20,9 @@ export function BuildCRUDController<
   T extends GenericPersistentEntity,
   D extends DeepPartial<T>,
 >(E: new () => T) {
-  const GenericService = BuildCRUDService<T, D>(E);
   class GenericController implements IGenericController<T, D> {
-    constructor(
-      @Inject(GenericService) readonly service: IGenericService<T, D>,
-    ) {}
+    constructor(@Inject() readonly service: IGenericService<T, D>) {}
+
     @Post()
     create(@Body() body: D) {
       return this.service.create(body);
