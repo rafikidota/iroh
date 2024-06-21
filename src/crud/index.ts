4efe944c8f2cd51199be1bbd78992dd643be4d9c
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-21 00:11:23 -0400

    revert: :fire: remove entity middleware

--- src/crud/index.ts
+++ src/crud/index.ts
@@ -4,5 +4,4 @@ export * from './dto';
 export * from './entity';
 export * from './guards';
 export * from './logger';
-export * from './middlewares';
 export * from './services';
--- src/crud/middlewares/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './entity.middleware';
-export * from './util/entity.middleware.factory';
