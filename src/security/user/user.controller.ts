654a208be09b02c36b901efb4b53c7c77a42fe1c
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-22 13:30:38 -0400

    feat: :construction: add generic user resources

--- src/security/user/user.controller.ts
+++ src/security/user/user.controller.ts
@@ -1,49 +1,12 @@
-import {
-  Post,
-  Body,
-  Get,
-  Query,
-  Param,
-  Patch,
-  HttpCode,
-  Delete,
-} from '@nestjs/common';
-import { DeepPartial } from 'typeorm';
-import { DefaultDto, LoggerOptions, SearchPaginateDto } from '../../crud';
+import { BuildGenericController } from '../../crud';
 import { GenericUser } from './user.generic';
 import { GenericUserService } from './user.service';
-import { User } from './decorators';
 
-export class GenericUserController<
-  User extends GenericUser,
-  CreateUserDto extends DefaultDto,
-> {
-  constructor(readonly service: GenericUserService<User, CreateUserDto>) {}
-
-  @Post()
-  create(@Body() body: DeepPartial<CreateUserDto>) {
-    return this.service.create(body);
+export function BuildGenericUserController() {
+  class GenericUserController extends BuildGenericController(GenericUser) {
+    constructor(readonly service: GenericUserService) {
+      super(service);
+    }
   }
-
-  @Get()
-  paginate(@Query() query: SearchPaginateDto) {
-    return this.service.paginate(query);
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    const options: LoggerOptions = { logging: true };
-    return this.service.findOne(id, options);
-  }
-
-  @Patch(':id')
-  update(@User() user: User, @Body() body: Partial<CreateUserDto>) {
-    return this.service.update(user, body);
-  }
-
-  @Delete(':id')
-  @HttpCode(204)
-  remove(@User() user: User) {
-    return this.service.remove(user.id);
-  }
+  return GenericUserController;
 }
--- src/security/user/user.service.ts
+++ src/security/user/user.service.ts
@@ -1,43 +1,6 @@
-import {
-  DefaultDto,
-  LoggerOptions,
-  GenericLogger,
-  SearchPaginateDto,
-} from '../../crud';
-import { Repository, DeepPartial } from 'typeorm';
+import { Injectable } from '@nestjs/common';
 import { GenericUser } from './user.generic';
+import { BuildGenericService } from '../../crud';
 
-export class GenericUserService<
-  User extends GenericUser,
-  CreateUserDto extends DefaultDto = DefaultDto,
-> {
-  protected logger: GenericLogger;
-  private context: string;
-  constructor(
-    protected readonly repository: Repository<User> & Repository<GenericUser>,
-  ) {
-    this.context = `${repository.metadata.name}Logger`;
-    this.logger = new GenericLogger(this.context);
-  }
-  async create(createDto: DeepPartial<CreateUserDto>) {
-    console.log(createDto);
-  }
-
-  async paginate(query: SearchPaginateDto) {
-    console.log(query);
-  }
-
-  async findAll() {}
-
-  async findOne(id: string, options: LoggerOptions) {
-    console.log(id, options);
-  }
-
-  async update(entity: User, updateDto: Partial<CreateUserDto>) {
-    console.log(entity, updateDto);
-  }
-
-  async remove(id: string) {
-    console.log(id);
-  }
-}
+@Injectable()
+export class GenericUserService extends BuildGenericService(GenericUser) {}
