45a7b4c41c51a2e57a59b866c33ae2040ddf2182
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-22 19:11:25 -0400

    fix: :construction_worker: add logs

--- src/security/user/entity/user.generic.ts
+++ src/security/user/entity/user.generic.ts
@@ -22,20 +22,28 @@ export class GenericUser extends GenericPersistentEntity {
   password: string;
 
   public verifyPassword(password: string) {
+    console.log('Hashing password...', {
+      password,
+      pass: this.password,
+    });
     return bcrypt.compare(password, this.password);
   }
 
-  protected async hashPassword() {
+  public async hashPassword() {
+    console.log('Hashing password...', this.password);
     this.password = await bcrypt.hash(this.password, 10);
+    console.log('Password hashed:', this.password);
   }
 
   @BeforeInsert()
   public async beforeInsert() {
+    console.log('Before insert hook');
     await this.hashPassword();
   }
 
   @BeforeUpdate()
   public async beforeUpdate() {
+    console.log('Before update hook');
     if (this.password) {
       await this.hashPassword();
     }
