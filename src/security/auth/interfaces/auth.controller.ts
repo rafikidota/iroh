0937aa8e935478022db3a757aef08a9bf5c5782d
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-22 14:11:05 -0400

    feat: :bug: auth interfaces returns

--- src/security/auth/interfaces/auth.controller.ts
+++ src/security/auth/interfaces/auth.controller.ts
@@ -1,7 +1,7 @@
 import { ISignInResponse } from './sign-in-response';
 
 export type IGenericAuthController<T> = {
-  signup(user: T): Promise<ISignInResponse>;
-  signin(user: T): Promise<Partial<T>>;
-  signout(user: T): Promise<Partial<T>>;
+  signup(user: T): Promise<Partial<T>>;
+  signin(user: T): Promise<ISignInResponse>;
+  signout(user: T): Promise<void>;
 };
