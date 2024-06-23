652736b6ad549414b1811f3c728bf8c03d4d20b0
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-22 22:24:29 -0400

    fix: :bug: apply decorator

--- src/security/auth/decorators/auth-entity-guard.decorator.ts
+++ src/security/auth/decorators/auth-entity-guard.decorator.ts
@@ -1,20 +1,33 @@
-import { UseGuards, Type } from '@nestjs/common';
+import {
+  UseGuards,
+  Type,
+  Injectable,
+  mixin,
+  ExecutionContext,
+  applyDecorators,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GenericUser } from './../../user/entity/user.generic';
 import { BuildBasicAuthGuard } from '../guards';
 import { AUTH_ENTITY_KEY } from './auth-entiy.decorator';
 
-export const AuthEntityGuard = <T extends GenericUser>() => {
-  return (target: unknown, key: string, descriptor: PropertyDescriptor) => {
-    const reflector = new Reflector();
-    const AuthEntity = reflector.get<Type<T>>(
-      AUTH_ENTITY_KEY,
-      target.constructor,
-    );
-    if (AuthEntity) {
-      UseGuards(BuildBasicAuthGuard(AuthEntity))(target, key, descriptor);
-    }
+export function AuthEntityGuard<T extends GenericUser>() {
+  @Injectable()
+  class MixinAuthGuard {
+    constructor(private reflector: Reflector) {}
 
-    return descriptor;
-  };
-};
+    canActivate(context: ExecutionContext) {
+      const target = context.getHandler();
+      const AuthEntity = this.reflector.get<Type<T>>(
+        AUTH_ENTITY_KEY,
+        target.constructor,
+      );
+
+      if (AuthEntity) {
+        return applyDecorators(UseGuards(BuildBasicAuthGuard<T>(AuthEntity)));
+      }
+    }
+  }
+
+  return UseGuards(mixin(MixinAuthGuard));
+}
