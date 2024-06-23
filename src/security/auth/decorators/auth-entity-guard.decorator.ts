6d3eb4fa90c9f3071d1706bf4d30140e57d61b64
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-22 22:07:10 -0400

    fix: :bug: auth entity guard

--- src/security/auth/decorators/auth-entity-guard.decorator.ts
+++ src/security/auth/decorators/auth-entity-guard.decorator.ts
@@ -1,16 +1,20 @@
-import { UseGuards } from '@nestjs/common';
+import { UseGuards, Type } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { GenericUser } from './../../user/entity/user.generic';
 import { BuildBasicAuthGuard } from '../guards';
 import { AUTH_ENTITY_KEY } from './auth-entiy.decorator';
 
-export const AuthEntityGuard =
-  () => (target: unknown, key: string, descriptor: PropertyDescriptor) => {
+export const AuthEntityGuard = <T extends GenericUser>() => {
+  return (target: unknown, key: string, descriptor: PropertyDescriptor) => {
     const reflector = new Reflector();
-    const AuthEntity = reflector.get(AUTH_ENTITY_KEY, target.constructor);
-
+    const AuthEntity = reflector.get<Type<T>>(
+      AUTH_ENTITY_KEY,
+      target.constructor,
+    );
     if (AuthEntity) {
       UseGuards(BuildBasicAuthGuard(AuthEntity))(target, key, descriptor);
     }
 
     return descriptor;
   };
+};
