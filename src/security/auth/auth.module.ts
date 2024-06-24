b106480a564300ddba544069feadc88af3e21e30
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-24 15:48:38 -0400

    feat: :sparkles: add auth and user module

---
+++ src/security/auth/auth.module.ts
@@ -0,0 +1,39 @@
+import { DynamicModule, Module, Type, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { DeepPartial } from 'typeorm';
+import { GenericUser } from '../user';
+import { JwtStrategy } from './strategies';
+import type { IGenericAuthController, IGenericAuthService } from './interfaces';
+import type { IGenericService } from '../../crud';
+export function BuildGenericAuthModule<
+  T extends GenericUser,
+  D extends DeepPartial<T>,
+  C extends IGenericAuthController<T>,
+  A extends IGenericAuthService<T>,
+  U extends IGenericService<T, D>,
+>(
+  User: Type<T>,
+  AuthController: Type<C>,
+  AuthService: Type<A>,
+  UserService: Type<U>,
+  UserModule: DynamicModule | Promise<DynamicModule>,
+) {
+  @Module({
+    controllers: [AuthController],
+    providers: [AuthService, UserService, JwtStrategy],
+    imports: [
+      TypeOrmModule.forFeature([User]),
+      forwardRef(() => UserModule),
+      PassportModule.register({ defaultStrategy: 'jwt' }),
+      JwtModule.register({
+        secret: process.env.JWT_SECRET || 'defaultSecret', // Asegúrate de usar una variable de entorno
+        signOptions: { expiresIn: '72h' },
+      }),
+    ],
+    exports: [TypeOrmModule, JwtModule, PassportModule, JwtStrategy],
+  })
+  class AuthModule {}
+  return AuthModule;
+}
---
+++ src/security/user/user.module.ts
@@ -0,0 +1,26 @@
+import { DynamicModule, Module, Type } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GenericUser } from './entity';
+import { DeepPartial } from 'typeorm';
+import type { IGenericAuthController } from '../auth';
+import type { IGenericService } from '../../crud';
+export function BuildGenericAuthModule<
+  T extends GenericUser,
+  D extends DeepPartial<T>,
+  C extends IGenericAuthController<T>,
+  U extends IGenericService<T, D>,
+>(
+  User: Type<T>,
+  UserController: Type<C>,
+  UserService: Type<U>,
+  AuthModule: DynamicModule | Promise<DynamicModule>,
+) {
+  @Module({
+    controllers: [UserController],
+    providers: [UserService],
+    imports: [TypeOrmModule.forFeature([User]), AuthModule],
+    exports: [TypeOrmModule, UserService],
+  })
+  class UserModule {}
+  return UserModule;
+}
