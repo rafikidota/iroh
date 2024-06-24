63d2c43a728649da56926935d2856a659b293d22
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-24 01:58:32 -0400

    revert: :fire: remove static auth

--- src/crud/controllers/generic.controller.ts
+++ src/crud/controllers/generic.controller.ts
@@ -8,9 +8,7 @@ import {
   Delete,
   HttpCode,
 } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
 import { DeepPartial } from 'typeorm';
-import { Auth } from '../../security';
 import { Entity, EntityGuard } from '../decorators';
 import { GenericPersistentEntity } from '../entity';
 import { LoggerOptions } from '../services';
@@ -25,38 +23,28 @@ export function BuildGenericController<
     constructor(readonly service: IGenericService<T, D>) {}
 
     @Post()
-    @Auth()
-    @ApiBearerAuth()
     create(@Body() body: D) {
       return this.service.create(body);
     }
 
     @Get()
-    @Auth()
-    @ApiBearerAuth()
     paginate(@Query() query: SearchPaginateDto) {
       return this.service.paginate(query);
     }
 
     @Get(':id')
-    @Auth()
-    @ApiBearerAuth()
     findOne(@Param('id') id: string) {
       const options: LoggerOptions = { logging: true };
       return this.service.findOne(id, options);
     }
 
     @Patch(':id')
-    @Auth()
-    @ApiBearerAuth()
     @EntityGuard(E)
     update(@Entity() entity: T, @Body() body: Partial<D>) {
       return this.service.update(entity, body);
     }
 
     @Delete(':id')
-    @Auth()
-    @ApiBearerAuth()
     @EntityGuard(E)
     @HttpCode(204)
     remove(@Entity() entity: T) {
