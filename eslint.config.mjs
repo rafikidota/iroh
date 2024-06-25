9f2a3e5e43170fcbbb84f44aee419e8b411552a2
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-24 22:30:04 -0400

    Revert "chore: :rotating_light: fix eslint config"
    This reverts commit 495bc66e675d10da4669db4202d19dd99762ebea.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,50 +1,43 @@
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import prettierPlugin from 'eslint-plugin-prettier';
-import importPlugin from 'eslint-plugin-import';
-import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import _import from "eslint-plugin-import";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
-// Obtener el nombre de archivo y directorio actuales
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-// Configuración de compatibilidad
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
 });
 
-export default [
-  {
-    ignores: ['**/node_modules/', '**/dist/'],
-  },
-  ...fixupConfigRules(
-    compat.extends(
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier',
-      'plugin:import/errors',
-      'plugin:import/warnings'
-    )
-  ),
-  {
+export default [{
+    ignores: ["**/node_modules/", "**/dist/"],
+}, ...fixupConfigRules(compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+)), {
     plugins: {
-      '@typescript-eslint': fixupPluginRules(typescriptEslint),
-      prettier: fixupPluginRules(prettierPlugin),
-      import: fixupPluginRules(importPlugin),
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+        prettier,
+        import: fixupPluginRules(_import),
     },
+
     languageOptions: {
-      parser: tsParser,
+        parser: tsParser,
     },
+
     rules: {
-      'prettier/prettier': 'error',
-      'import/no-absolute-path': 'error',
-      '@typescript-eslint/no-explicit-any': 'off',
+        "prettier/prettier": "error",
+        "import/no-absolute-path": "error",
+        "@typescript-eslint/no-explicit-any": "off",
     },
-  },
-];
+}];
