3b33feb8d1f805e4a0016fe017587f1129b2407d
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-24 20:54:10 -0400

    chore: :rotating_light: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,27 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-
-export default [
-  {languageOptions: { globals: globals.browser }},
+const config = [
+  {
+    languageOptions: { globals: globals.browser },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'import': importPlugin,
+      'prettier': prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'import/no-absolute-path': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
 ];
+
+export default config;
