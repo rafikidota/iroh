e605093644b9eae62c072bd9baf22eb4cb06ab25
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-26 00:35:05 -0400

    chore: :rotating_light: remove schematics from eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,10 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+    ignores: ['src/schematics/**/*.ts'],
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
