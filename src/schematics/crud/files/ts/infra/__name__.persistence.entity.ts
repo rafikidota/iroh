93d43274f022c92c4d79d8426731dd551f81c9c3
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-26 00:09:38 -0400

    revert: :fire: remove unnecessary stuff

--- src/schematics/crud/files/ts/infra/__name__.persistence.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Entity } from 'typeorm'
-import { MinimalEntity } from '@guachos/nestjs-typeorm-data-access'
-
-@Entity('<%= lowerCase(name) %>')
-export class <%= classify(name) %>Persistence extends MinimalEntity {
-
-    //custom props typeorm persistence
-}
