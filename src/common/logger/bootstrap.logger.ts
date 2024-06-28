46a650d115e272cf5bb80d70d3213dd755a03c84
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-28 10:56:11 -0400

    chore: :loud_sound: add swagger to bootstrap logger

--- src/common/logger/bootstrap.logger.ts
+++ src/common/logger/bootstrap.logger.ts
@@ -2,5 +2,5 @@ import { Logger } from '@nestjs/common';
 
 export function BootstrapLogger(port: number) {
   const logger = new Logger('BootstrapLogger');
-  logger.log(`App running on port ${port} 🚀`);
+  logger.log(`App running on http://localhost:${port}/swagger 🚀`);
 }
