e024e69afd3520cfee5065367350ef80a8c00629
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-06-29 18:49:42 -0400

    chore: :recycle: update bootstrap logger

--- src/common/logger/bootstrap.logger.ts
+++ src/common/logger/bootstrap.logger.ts
@@ -1,6 +1,20 @@
 import { Logger } from '@nestjs/common';
 
-export function BootstrapLogger(port: number) {
+export interface BootstrapLoggerOptions {
+  port: number;
+  swagger?: string;
+}
+
+const getLog = (options: BootstrapLoggerOptions) => {
+  const { port, swagger } = options;
+  const url = 'http://localhost';
+  if (swagger) {
+    return `Swagger running on ${url}:${port}/${swagger} 🚀`;
+  }
+  return `App running on ${url}:${port} 🚀`;
+};
+
+export function BootstrapLogger(options: BootstrapLoggerOptions) {
   const logger = new Logger('BootstrapLogger');
-  logger.log(`App running on http://localhost:${port}/swagger 🚀`);
+  logger.log(getLog(options));
 }
