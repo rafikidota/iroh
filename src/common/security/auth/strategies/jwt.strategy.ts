d25f55311e7df3229cb5e78d4c24b0111adc07ed
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-07-07 18:49:30 -0400

    fix: :bug: throw error if secret is not defined

--- src/common/security/auth/strategies/jwt.strategy.ts
+++ src/common/security/auth/strategies/jwt.strategy.ts
@@ -2,14 +2,16 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import dotenv from 'dotenv';
-import { v4 as uuidv4 } from 'uuid';
 import { Payload } from '../interfaces';
 
 dotenv.config();
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
-    const secretOrKey = process.env.JWT_SECRET || uuidv4();
+    const secretOrKey = process.env.JWT_SECRET;
+    if (!secretOrKey) {
+      throw new Error('JWT_SECRET is not defined');
+    }
     const strategy = {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
