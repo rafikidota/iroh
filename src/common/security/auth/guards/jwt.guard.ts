62391ddc4739fcf8157d9a51129713609c4de18a
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-08-04 00:34:29 -0400

    refactor: :recycle: minor changes

--- src/common/security/auth/guards/jwt.guard.ts
+++ src/common/security/auth/guards/jwt.guard.ts
@@ -6,7 +6,9 @@ export function JwtAuthGuard() {
   class JwtAuthGuard extends AuthGuard('jwt') {
     canActivate(context: ExecutionContext): boolean {
       const request = context.switchToHttp().getRequest();
-      Object.assign(request, { startTime: Date.now() });
+      if (!request.startTime) {
+        request.startTime = Date.now();
+      }
       return super.canActivate(context) as boolean;
     }
   }
