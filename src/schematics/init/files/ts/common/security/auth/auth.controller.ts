6f462e23b3aadb0caa2bc749a398730353c908f6
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-08-18 00:06:41 -0400

    feat: :sparkles: add auth controller swagger, interceptor and exception filters

--- src/schematics/init/files/ts/common/security/auth/auth.controller.ts
+++ src/schematics/init/files/ts/common/security/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { GenericAuthController } from '@rafikidota/iroh';
 import { AuthService } from './auth.service';
 import { User } from '../user/entities/user.entity';
-import { ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from '../user/dto/user.create.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
-export class AuthController extends GenericAuthController(User) {
+export class AuthController extends GenericAuthController(User, CreateUserDto) {
   constructor(readonly service: AuthService) {
     super(service);
   }
