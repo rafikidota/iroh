0af25c547e933c2ada5ca1334db14ca2ed9f1cd3
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-08-21 23:11:02 -0400

    feat: :sparkles: add generic permission module

--- src/common/security/permission/index.ts
+++ src/common/security/permission/index.ts
@@ -2,5 +2,7 @@ export * from './decorators';
 export * from './entity';
 export * from './enum';
 export * from './guards';
+export * from './interfaces';
 export * from './permission.controller';
+export * from './permission.module';
 export * from './permission.service';
---
+++ src/common/security/permission/permission.module.ts
@@ -0,0 +1,46 @@
+import { Module, ModuleMetadata, OnModuleInit, Type } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { DeepPartial } from 'typeorm';
+import { GenericPermission } from './entity';
+import type {
+  IGenericController,
+  IGenericRepository,
+  IGenericService,
+} from 'crud';
+import type { IPermissionSeeder } from './interfaces';
+
+export function GenericPermissionModule<
+  T extends GenericPermission,
+  D extends DeepPartial<T>,
+  C extends IGenericController<T, D>,
+  S extends IGenericService<T, D>,
+>(
+  Permission: Type<T>,
+  PermissionController: Type<C>,
+  PermissionService: Type<S>,
+  PermissionRepository: Type<IGenericRepository<T, D>>,
+  PermissionSeeder: Type<IPermissionSeeder>,
+  AuthModule: Type<ModuleMetadata>,
+) {
+  @Module({
+    controllers: [PermissionController],
+    providers: [PermissionService, PermissionRepository, PermissionSeeder],
+    imports: [
+      TypeOrmModule.forFeature([Permission]),
+      AuthModule,
+      ConfigModule,
+      HttpModule,
+    ],
+    exports: [TypeOrmModule, PermissionService],
+  })
+  class PermissionModule implements OnModuleInit {
+    constructor(readonly seeder: IPermissionSeeder) {}
+
+    async onModuleInit() {
+      this.seeder.synchronize();
+    }
+  }
+  return PermissionModule;
+}
