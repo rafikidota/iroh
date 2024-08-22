fedab661066a79826888d4aa1a9c609a2897e45f
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-08-21 23:11:48 -0400

    feat: :sparkles: add permission and role modules to security module schematic

--- src/schematics/init/files/ts/common/security/security.module.ts
+++ src/schematics/init/files/ts/common/security/security.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PermissionModule } from './permission/permission.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, PermissionModule, RoleModule],
 })
 export class SecurityModule {}
