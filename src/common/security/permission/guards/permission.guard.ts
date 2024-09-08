8a52025706965ea290f1b8e915138d8bca84ab52
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-08 17:34:13 -0400

    feat: :sparkles: check no permission needed on permission guard

--- src/common/security/permission/guards/permission.guard.ts
+++ src/common/security/permission/guards/permission.guard.ts
@@ -1,3 +1,4 @@
+import { Reflector } from '@nestjs/core';
 import {
   CanActivate,
   ExecutionContext,
@@ -7,14 +8,25 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { GenericPermission } from '../entity';
+import { NoPermissionNeededKey } from '../decorators';
 
 export function BuildPermissionGuard<T extends GenericPermission>(E: Type<T>) {
   @Injectable()
   class PermissionGuard implements CanActivate {
-    constructor(@InjectRepository(E) readonly repository: Repository<T>) {}
+    constructor(
+      @InjectRepository(E)
+      readonly repository: Repository<T>,
+      readonly reflector: Reflector,
+    ) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
-      return context ? true : false;
+      const handler = context.getHandler();
+      const NoPermissionNeeded = this.reflector.get<boolean>(
+        NoPermissionNeededKey,
+        handler,
+      );
+
+      return NoPermissionNeeded ? true : false;
     }
   }
   return PermissionGuard;
