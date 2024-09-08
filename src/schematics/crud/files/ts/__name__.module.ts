647aecf64eea437b4cb6ab624e418b3a0e0f2522
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-08 19:52:32 -0400

    feat: :sparkles: import security module on generic modules

--- src/schematics/crud/files/ts/__name__.module.ts
+++ src/schematics/crud/files/ts/__name__.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from '../../common/security/auth/auth.module';
+import { SecurityModule } from '../../common/security/security.module';
 import { <%= classify(name) %> } from './entities/<%= lowerCase(name) %>.entity';
 import { <%= classify(name) %>Controller } from './<%= lowerCase(name) %>.controller';
 import { <%= classify(name) %>Service } from './<%= lowerCase(name) %>.service';
@@ -9,7 +9,7 @@ import { <%= classify(name) %>Repository } from './<%= lowerCase(name) %>.reposi
 @Module({
   controllers: [<%= classify(name) %>Controller],
   providers: [<%= classify(name) %>Service, <%= classify(name) %>Repository],
-  imports: [TypeOrmModule.forFeature([<%= classify(name) %>]), AuthModule],
+  imports: [TypeOrmModule.forFeature([<%= classify(name) %>]), SecurityModule],
   exports: [TypeOrmModule],
 })
 export class <%= classify(name) %>Module {}
