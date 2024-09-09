836086a66f3fa7f09df31a0614c5367d539c59a0
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-08 22:20:36 -0400

    feat: :sparkles: add user and role relation

--- src/schematics/init/files/ts/common/security/role/entities/role.entity.ts
+++ src/schematics/init/files/ts/common/security/role/entities/role.entity.ts
@@ -1,5 +1,9 @@
-import { Entity } from 'typeorm';
+import { Entity, OneToMany } from 'typeorm';
 import { GenericRole } from '@rafikidota/iroh';
+import { User } from '../../user/entities/user.entity';
 
 @Entity()
-export class Role extends GenericRole {}
+export class Role extends GenericRole {
+  @OneToMany(() => User, (user) => user.role)
+  users: User[];
+}
--- src/schematics/init/files/ts/common/security/user/entities/user.entity.ts
+++ src/schematics/init/files/ts/common/security/user/entities/user.entity.ts
@@ -1,5 +1,9 @@
 import { GenericUser } from '@rafikidota/iroh';
-import { Entity } from 'typeorm';
+import { Entity, ManyToOne } from 'typeorm';
+import { Role } from '../../role/entities/role.entity';
 
 @Entity()
-export class User extends GenericUser {}
+export class User extends GenericUser {
+  @ManyToOne(() => Role, (role) => role.users, { eager: true })
+  role: Role;
+}
