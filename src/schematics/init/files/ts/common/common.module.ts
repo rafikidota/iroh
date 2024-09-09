edc2d711a8390aac67dafafc058a8167fa47c651
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-08 21:32:00 -0400

    feat: :sparkles: add config module to common module

--- src/schematics/init/files/ts/common/common.module.ts
+++ src/schematics/init/files/ts/common/common.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SecurityModule } from './security/security.module';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
-  imports: [SecurityModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema
+    }),
+    SecurityModule
+  ],
 })
-export class CommonModule {}
+export class CommonModule { }
