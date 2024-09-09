9bdbda5fd18ced193b53bf00800d84c555c9e909
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-09 00:53:03 -0400

    feat: :sparkles: add role and permission many to many

--- src/schematics/init/files/ts/common/security/role/entities/role.entity.ts
+++ src/schematics/init/files/ts/common/security/role/entities/role.entity.ts
@@ -1,9 +1,24 @@
-import { Entity, OneToMany } from 'typeorm';
+import { Entity, JoinTable, ManyToMany, OneToMany } from 'typeorm';
 import { GenericRole } from '@rafikidota/iroh';
 import { User } from '../../user/entities/user.entity';
+import { Permission } from '../../permission/entities/permission.entity';
 
 @Entity()
 export class Role extends GenericRole {
   @OneToMany(() => User, (user) => user.role)
   users: User[];
+
+  @ManyToMany(() => Permission, (permission) => permission.roles)
+  @JoinTable({
+    name: 'role_permission',
+    joinColumn: {
+      name: 'role_id',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'permission_id',
+      referencedColumnName: 'id',
+    },
+  })
+  permissions: Permission[];
 }
