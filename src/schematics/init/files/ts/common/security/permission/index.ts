6e9edd6a1950a8764cf685540c7b5e595f1c0a37
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-10 02:10:54 -0400

    chore: :sparkles: add permission index.ts

---
+++ src/schematics/init/files/ts/common/security/permission/index.ts
@@ -0,0 +1,7 @@
+export * from './dto';
+export * from './entities';
+export * from './permission.controller';
+export * from './permission.module';
+export * from './permission.repository';
+export * from './permission.seeder';
+export * from './permission.service';
