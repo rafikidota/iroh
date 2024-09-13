12f79116dfd2f0d92bd77b7ea8c074da9e566449
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-13 03:08:37 -0400

    fix: :bug: auth controller schematic

--- src/schematics/init/files/ts/common/security/auth/auth.controller.ts
+++ src/schematics/init/files/ts/common/security/auth/auth.controller.ts
@@ -4,10 +4,16 @@ import { GenericAuthController } from '@rafikidota/iroh';
 import { AuthService } from './auth.service';
 import { User } from '../user/entities/user.entity';
 import { CreateUserDto } from '../user/dto/user.create.dto';
+import { UserView } from '../user/entities/user.view';
+
 
 @ApiTags('Auth')
 @Controller('auth')
-export class AuthController extends GenericAuthController(User, CreateUserDto) {
+export class AuthController extends GenericAuthController(
+  User,
+  CreateUserDto,
+  UserView,
+) {
   constructor(readonly service: AuthService) {
     super(service);
   }
