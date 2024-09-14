46b5125a03f80d6e87c79fe09cde59ed80b83f70
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-14 19:14:21 -0400

    chore: :sparkles: add some dto index ts

---
+++ src/schematics/crud/files/ts/infra/index.ts
@@ -0,0 +1,4 @@
+export * from './<%= lowerCase(name) %>.mapper';
+export * from './<%= lowerCase(name) %>.persistent';
+export * from './<%= lowerCase(name) %>.repository';
+export * from './<%= lowerCase(name) %>.view';
