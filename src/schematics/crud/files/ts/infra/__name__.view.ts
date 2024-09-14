4f8a3d16f94419b5da3cdad4e9d6a1f2dbe12b37
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-14 18:45:15 -0400

    feat: :sparkles: improve generic view schematic

--- src/schematics/crud/files/ts/infra/__name__.view.ts
+++ src/schematics/crud/files/ts/infra/__name__.view.ts
@@ -1,31 +1,28 @@
 import {
-    ApiProperty,
-    IntersectionType,
-    OmitType,
-    PartialType,
-  } from '@nestjs/swagger';
-  import { GenericView } from '@rafikidota/iroh';
-  import { Create<%= classify(name) %>Dto } from '../app/dto/<%= lowercase(name) %>.create.dto';
-  import { <%= classify(name) %>Domain } from '../domain/<%= lowercase(name) %>.domain';
-  
-  export class <%= classify(name) %>View extends OmitType(
-    IntersectionType(PartialType(Create<%= classify(name) %>Dto), GenericView),
-    [],
-  ) {
-    @ApiProperty()
-    id: string;
-    @ApiProperty()
-    name: string;
-    @ApiProperty()
-    createdAt: Date;
-    @ApiProperty()
-    updatedAt: Date;
-  
-    constructor(domain: <%= classify(name) %>Domain) {
-      super(domain);
-      this.id = domain.id;
-      this.createdAt = domain.createdAt;
-      this.updatedAt = domain.updatedAt;
-    }
+  ApiProperty,
+  IntersectionType,
+  OmitType,
+  PartialType,
+} from '@nestjs/swagger';
+import { GenericView } from '@rafikidota/iroh';
+import { Create<%= classify(name) %>Dto } from '../app/dto/<%= lowerCase(name) %>.create.dto';
+import { <%= classify(name) %>Domain } from '../domain/<%= lowerCase(name) %>.domain';
+
+export class <%= classify(name) %>View extends OmitType(
+  IntersectionType(PartialType(Create<%= classify(name) %>Dto), GenericView),
+  [],
+) {
+  @ApiProperty()
+  id: string;
+  @ApiProperty()
+  createdAt: Date;
+  @ApiProperty()
+  updatedAt: Date;
+
+  constructor(domain: <%= classify(name) %>Domain) {
+    super(domain);
+    this.id = domain.id;
+    this.createdAt = domain.createdAt;
+    this.updatedAt = domain.updatedAt;
   }
-  
+}
