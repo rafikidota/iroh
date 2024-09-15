1b665d2ca23b84a97df85d5cb268f3d59426779c
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-14 23:18:31 -0400

    fix: :bug: permission persistent import

--- src/schematics/crud/files/ts/__name__.controller.ts
+++ src/schematics/crud/files/ts/__name__.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { GenericController, SecurityGuard } from '@rafikidota/iroh';
-import { Permission } from '../../common/security/permission';
+import { PermissionPersistent } from '../../common/security/permission';
 import { <%= classify(name) %>Service } from './<%= lowerCase(name) %>.service';
 import { <%= classify(name) %>Persistent } from './infra/<%= lowerCase(name) %>.persistent';
 import { Create<%= classify(name) %>Dto, Update<%= classify(name) %>Dto } from './app/dto';
@@ -10,7 +10,7 @@ import { <%= classify(name) %>View } from './infra/<%= lowerCase(name) %>.view';
 @ApiBearerAuth()
 @ApiTags('<%= classify(name) %>')
 @Controller('<%= dasherize(name) %>')
-@SecurityGuard(Permission)
+@SecurityGuard(PermissionPersistent)
 export class <%= classify(name) %>Controller extends GenericController(
   <%= classify(name) %>Persistent,
   Create<%= classify(name) %>Dto,
