0b545473abc95caff528ec62f04aeb1468428757
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-14 20:14:31 -0400

    fix: :bug: user controller schematics

--- src/schematics/init/files/ts/common/security/user/user.controller.ts
+++ src/schematics/init/files/ts/common/security/user/user.controller.ts
@@ -1,16 +1,22 @@
 import { Controller } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { AuthGuard, GenericUserController } from '@rafikidota/iroh';
+import { GenericUserController, SecurityGuard } from '@rafikidota/iroh';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/user.create.dto';
 import { UpdateUserDto } from './dto/user.update.dto';
+import { UserView } from './entities/user.view';
 
-@AuthGuard()
 @ApiBearerAuth()
-@ApiTags(User.name)
-@Controller(User.name.toLowerCase())
-export class UserController extends GenericUserController(User, CreateUserDto, UpdateUserDto) {
+@ApiTags('User')
+@Controller('user')
+@SecurityGuard()
+export class UserController extends GenericUserController(
+  User,
+  CreateUserDto,
+  UpdateUserDto,
+  UserView,
+) {
   constructor(readonly service: UserService) {
     super(service);
   }
