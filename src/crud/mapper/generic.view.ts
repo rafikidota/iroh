924dc8d35839aaba66aafffdc0528ffa2f63404d
rafiki <71207415+rafikidota@users.noreply.github.com>
2024-09-25 03:03:32 -0400

    docs: :memo: add api property details

--- src/crud/mapper/generic.view.ts
+++ src/crud/mapper/generic.view.ts
@@ -1,5 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { v4 as uuid } from 'uuid';
+
 export class GenericView {
+  @ApiProperty({
+    example: uuid(),
+  })
   id: string;
+
+  @ApiProperty({
+    type: Date,
+    example: new Date(),
+  })
   createdAt: Date;
+
+  @ApiProperty({
+    type: Date,
+    example: new Date(),
+  })
   updatedAt: Date;
 }
